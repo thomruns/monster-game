68b96b01f5cc9beeda72c77964efb3b5c1e63b4e
Thom David <thomdavid74@gmail.com>
2018-08-28 16:31:16 -0700

    L52 Refactor attack functionality

--- app.js
+++ app.js
@@ -12,29 +12,13 @@ new Vue({
       this.monsterHealth = 100
     },
     attack: function() {
-      console.log('Attack!')
-      var max = 10
-      var min = 3
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min) // random damage
-      this.monsterHealth -= damage // damage to monster
-
-      if (this.monsterHealth <= 0) {
-        alert('You won!')
-        this.gameIsRunning = false
+      this.monsterHealth -= this.calculateDamage(3, 10) // damage to monster
+      if (this.checkWin()) {
         return
       }
 
-      console.log(`Monster is damaged by ${damage}`)
-      max = 12
-      min = 5
-      damage = Math.max(Math.floor(Math.random() * max) + 1, min) // monster damage
-      this.playerHealth -= damage // damage to player
-
-      if (this.playerHealth <= 0) {
-        alert('You lost!')
-        this.gameIsRunning = false
-      }
-      console.log(`Player is damaged by ${damage}`)
+      this.playerHealth -= this.calculateDamage(5, 12) // damage to player
+      this.checkWin()
     },
     specialAttack: function() {
       console.log('SPECIAL ATTACK!')
@@ -44,6 +28,27 @@ new Vue({
     },
     giveUp: function() {
       this.gameIsRunning = false
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New Game?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! New Game?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      }
+      return false
     }
-  }
+  } // end methods
 })
